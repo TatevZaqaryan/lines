7508a9a7233038a562b64afc07a1c9d53d59c0cf
Aramayis Sargsyan <mayissargsyan1995@gmail.com>
2021-10-22 18:41:59 +0400

    wip

--- src/score.ts
+++ src/score.ts
@@ -1,28 +1,38 @@
-import {Text } from 'pixi.js';
+import { Text, TextStyle } from 'pixi.js';
 
 export class Score extends Text {
-    constructor(){
-        super();
+    constructor() {
+        super(`High score: 0`, {
+            fontSize: 20,
+            fill: ['#ffffff', '#666666'],
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 3,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 5,
+        });
     }
-    
-    getScore(){
-        console.warn(7);
-        let score = 0
+
+    getScore() {
+        // console.warn(7);
+        // let score = 0
         // const style = new TextStyle({
-        //     fontSize: 36
-        // })
-        console.log(Score);
-        
-        const basicText = new Text(`High score: ${score}`);
-        console.log(basicText);
-        
-
-        basicText.pivot.x =  basicText.width/2;
-        basicText.pivot.y =  basicText.height/2;
-
-        this.addChild(basicText);
-        
-
-     
+        //     fontSize: 20,
+        //     fill: ['#ffffff', '#666666'],
+        //     dropShadow: true,
+        //     dropShadowColor: '#000000',
+        //     dropShadowBlur: 3,
+        //     dropShadowAngle: Math.PI / 6,
+        //     dropShadowDistance: 5,
+        //       })
+        //            console.log(Score);
+        // console.log(this);
+        // const basicText = new Text()
+        // console.log(basicText);
+        // basicText.text =`High score: ${score}00`
+        // basicText.style = style
+        // basicText.pivot.x =  basicText.width/2;
+        // basicText.pivot.y =  basicText.height/2;
+        // this.addChild(basicText);
     }
 }
